750f858d01b8883f8df0d1bab80aab2dbfcf98ab
galaxysha512 <galaxysha512@gmail.com>
2020-03-11 20:23:30 +0800

    commit

--- v2+tls.sh
+++ v2+tls.sh
@@ -8,39 +8,34 @@ read domain
 
 sleep 2
 
-yum -y install yum-utils
-yum -y install epel-release
-
-yum -y install wget unzip
-yum -y install libtool perl-core zlib-devel gcc pcre*
+sudo yum -y install libtool wget unzip gcc gcc-c++ pcre pcre-devel openssl openssl-devel zlib zlib-devel perl-core
 
 sleep 2
 
-cd ~
 mkdir v2ray
 wget https://github.com/v2ray/v2ray-core/releases/download/v4.22.1/v2ray-linux-64.zip -P ./v2ray
 unzip ./v2ray/v2ray-linux-64.zip -d ./v2ray
 
 sleep 2
 
-sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
-setenforce 0
+sudo su -c "sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config"
+sudo setenforce 0
 
 sleep 2
+
+sudo useradd -s /bin/false -M nginx
+wget https://nginx.org/download/nginx-1.15.8.tar.gz
+tar xf nginx-1.15.8.tar.gz && rm nginx-1.15.8.tar.gz
+
+cd nginx-1.15.8
 
 wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz
 tar xzvf openssl-1.1.1d.tar.gz
 
-sleep 2
-
-useradd nginx
-wget https://nginx.org/download/nginx-1.16.1.tar.gz
-tar xf nginx-1.16.1.tar.gz && rm nginx-1.16.1.tar.gz
-cd nginx-1.16.1
 ./configure    \
         --user=nginx    \
         --group=nginx    \
-        --with-openssl=../openssl-1.1.1d    \
+        --with-openssl=openssl-1.1.1d    \
         --with-openssl-opt='enable-tls1_3'    \
         --with-http_v2_module    \
         --with-http_ssl_module    \
@@ -49,28 +44,26 @@ cd nginx-1.16.1
         --with-http_sub_module    \
         --with-stream    \
         --with-stream_ssl_module
-make && make install
+make
+sudo make install
 
-/usr/local/nginx/sbin/nginx
+sudo /usr/local/nginx/sbin/nginx
 
 curl https://get.acme.sh | sh
-    ~/.acme.sh/acme.sh  --issue  -d $domain  --webroot /usr/local/nginx/html/
-    ~/.acme.sh/acme.sh  --installcert  -d  $domain   \
-        --key-file   /usr/local/nginx/conf/$domain.key \
-        --fullchain-file /usr/local/nginx/conf/fullchain.cer \
-        --reloadcmd  "service nginx force-reload"
+~/.acme.sh/acme.sh --issue -d $domain --webroot /usr/local/nginx/html/
+~/.acme.sh/acme.sh --installcert -d  $domain --key-file /usr/local/nginx/conf/$domain.key --fullchain-file /usr/local/nginx/conf/fullchain.cer 
 
+sudo /usr/local/nginx/sbin/nginx -s reload
 
-
-cat > /usr/local/nginx/conf/nginx.conf <<-EOF
+sudo su -c 'cat > /etc/nginx/nginx.conf <<-EOF
 user nginx;
 worker_processes 1;
-pid        /usr/local/nginx/logs/nginx.pid;
+pid        /etc/nginx/nginx.pid;
 events {
     worker_connections  1024;
 }
 http {
-    include       /usr/local/nginx/conf/mime.types;
+    include       /etc/nginx/mime.types;
     default_type  application/octet-stream;
 
     sendfile        on;
@@ -88,8 +81,8 @@ http {
 		server_name $domain;
 		root /usr/local/nginx/html;
 		index index.php index.html;
-		ssl_certificate /usr/local/nginx/conf/fullchain.cer;
-		ssl_certificate_key /usr/local/nginx/conf/$domain.key;
+		ssl_certificate /etc/nginx/fullchain.cer;
+		ssl_certificate_key /etc/nginx/$domain.key;
 		#TLS 版本控制
 		ssl_protocols   TLSv1.3;
 		ssl_ciphers     TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256;
@@ -110,4 +103,4 @@ http {
 	
 	}
 }
-EOF
+EOF'
